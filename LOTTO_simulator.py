9909722a53382274e7bace8f8976a1cd92564c7a
Bartosz Wroblewski <wroobal@gmail.com>
2020-11-10 20:26:19 +0100

    Wprowadzenie mechanizmu losującego 6 liczb z 1-49

---
+++ LOTTO_simulator.py
@@ -0,0 +1,8 @@
+import random
+# list of number 1-49
+list_numbers = list(range(0, 50))
+# shuffeling list
+random.shuffle(list_numbers)
+#taking six numbers from shuffled list
+drawn_list = list_numbers[0:6]
+
