573363c9858db91b145ed2730f1ce6db1f198dca
Bartosz Wroblewski <wroobal@gmail.com>
2020-11-10 20:43:44 +0100

    Zapytanie użytkownika o 6 liczb

--- LOTTO_simulator.py
+++ LOTTO_simulator.py
@@ -5,4 +5,8 @@ list_numbers = list(range(0, 50))
 random.shuffle(list_numbers)
 #taking six numbers from shuffled list
 drawn_list = list_numbers[0:6]
+#sorting list
+drawn_list.sort()
 
+separator = "','"
+user_list = [input(f"Guess SIX numbers 1-49, write numbers using {separator} to seperate them:")]
