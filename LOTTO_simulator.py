79927df569b3bf493418737870cec90b6bb86d62
Bartosz Wroblewski <wroobal@gmail.com>
2020-11-14 13:40:45 +0100

    Wstępna wersja LOTTO

--- LOTTO_simulator.py
+++ LOTTO_simulator.py
@@ -1,6 +1,6 @@
 import random
 # list of number 1-49
-list_numbers = list(range(0, 50))
+list_numbers = list(range(1, 50))
 # shuffeling list
 random.shuffle(list_numbers)
 #taking six numbers from shuffled list
@@ -8,5 +8,42 @@ drawn_list = list_numbers[0:6]
 #sorting list
 drawn_list.sort()
 
-separator = "','"
-user_list = [input(f"Guess SIX numbers 1-49, write numbers using {separator} to seperate them:")]
+
+final_user_list= []
+
+while True:
+    try:
+        separator = "','"
+        user_list = (input(f"Guess SIX numbers 1-49, write numbers using {separator} to seperate them:"))
+        split_user_list = user_list.split(',')
+        split_user_list = [int(i) for i in split_user_list]
+        [final_user_list.append(x) for x in split_user_list]
+
+    except ValueError:
+        print("One or more arguments is not integer")
+
+    break
+
+final_user_list.sort()
+wynik = 0
+
+for liczba in final_user_list:
+    if final_user_list.count(liczba)>1:
+        print(f"Number {liczba} duplicated")
+        break
+    elif liczba not in range(0,50):
+        print(f'Number {liczba} is not in range  1-49')
+        break
+    else:
+        if drawn_list.count(liczba)==1:
+            wynik += 1
+
+if  final_user_list == []:
+        print("Nie wprowadziłeś poprawnie liczb")
+elif len(final_user_list) != 6:
+    print("< 6 elements")
+    pass
+else:
+    print(f'Your numbers {final_user_list}, \nDrawn numbers {drawn_list}\nYou guessed #{wynik}# of them !')
+
+
